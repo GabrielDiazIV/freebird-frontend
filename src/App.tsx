462882c8e1b6302aee3d30e40ffa8b56acc6d4ff
gabrieldiaziv <gabrieldiaziv@gmail.com>
2023-01-29 07:47:36 -0600

    ff

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,70 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import Leaderboard from "./Leaderboard";
 import { DataClient } from "./DataServiceClientPb";
-import { TweetStreamRequest, Bird } from "./data_pb";
+import { TweetStreamRequest, Bird, BirdsRequest, FreebirdEntity } from "./data_pb";
 import Card from "./Card";
+
+type BirdState = {
+  b: Bird,
+  twts: Date[]
+}
 
 function App() {
   const [toggle, setToggle] = useState(false);
   const [popping, setPopping] = useState(false);
+  const [birds, setBirds] = useState<Map<number, BirdState>>(new Map());
 
   const pop = () => {
     setPopping(true);
     setTimeout(() => setPopping(false), 500);
   };
 
-  const getData = (x: Bird) => console.log(x.getImgUrl());
+  const MINUTE = 60 * 1000
+
 
   useEffect(() => {
     const client = new DataClient("http://localhost:8080");
     const res = client.tweetStream(new TweetStreamRequest());
-    // @ts-ignore
-    const id = res.on("data", getData);
-	return () => {
-    // @ts-ignore
-		res.removeListener("data", getData)
-	}
-  }, []);
 
-  const buttonClass = function (active: boolean) {
+    const getData = (b: Bird) => {
+      pop()
+      const curr = birds.get(b.getId())
+      if (!curr) {
+        birds.set(b.getId(), { b, twts: [new Date()] })
+      } else {
+        const twts = curr.twts.filter((t: Date) => t.getTime() + MINUTE > Date.now());
+        twts.push(new Date())
+        birds.set(b.getId(), { b, twts })
+
+        setBirds(new Map(birds));
+      }
+    }
+    const fetchBootstrap = async () => {
+      const _birds = await client.getAllBirds(new BirdsRequest(), null);
+      console.log(_birds.getBirdsList())
+      _birds.getBirdsList().forEach((b) => birds.set(b.getId(), { b, twts: [new Date()] }));
+      setBirds(birds);
+    }
+
+    fetchBootstrap().catch((err) => {
+      console.log(err)
+    })
+
+    res.on("data", getData);
+    return () => {
+      res.removeListener("data", getData)
+    }
+  }, [birds]);
+
+  const score = (b: Bird) => {
+    return b.getNPositive() / (b.getNNegative() + 1)
+  }
+
+  const sortedBirds = () => {
+    return Array.from(birds.values()).sort((b1, b2) => score(b2.b) - score(b1.b))
+  }
+
+  const buttonClass = function(active: boolean) {
     return active
       ? "font-bold w-1/2 bg-white hover:bg-slate-100 bluesht transition-all duration-200"
       : "w-1/2 bg-white hover:bg-slate-100 bluesht-disabled transition-all";
@@ -45,7 +82,7 @@ function App() {
       apply sticky to buttons and ur good */}
       <header className="flex sticky top-0 z-50 flex-col items-center w-5/12 bg-white border-1">
         <div className="flex flex-col items-center w-full">
-          <img className="w-20 h-20" src={popSrc()} />
+          <img alt="yay" className="w-20 h-20" src={popSrc()} />
           <button onClick={(_) => pop()} className="ml-4 text-4xl font-bold">
             FreeBird
           </button>
@@ -74,12 +111,19 @@ function App() {
         </div>
       </header>
       <div className="flex relative flex-col w-5/12 bg-white border-t-0 border-b-0 border-1">
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+
+        {
+          sortedBirds().map((b: BirdState) => {
+            if (b.b.getEntityType() === FreebirdEntity.COMPANY && toggle)
+              return (
+                <Card key={b.b.getId()} bird={b.b} twts={b.twts} />
+              )
+            if (b.b.getEntityType() === FreebirdEntity.SHILL && !toggle)
+              return (
+                <Card key={b.b.getId()} bird={b.b} twts={b.twts} />
+              )
+          })
+        }
       </div>
     </div>
   );
