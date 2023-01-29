8225845b07a66aec140aff98016e2fe06a9b42cd
rynoson <sonlimryan@gmail.com>
2023-01-29 01:26:44 -0600

    afjdskflksdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,86 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import Leaderboard from "./Leaderboard";
-import Card from "./Card";
 import { DataClient } from "./DataServiceClientPb";
-import { TweetStreamRequest } from "./data_pb"
-
+import { TweetStreamRequest } from "./data_pb";
+import Card from "./Card";
 
 function App() {
-	const [toggle, setToggle] = useState(false);
-	const [popping, setPopping] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [popping, setPopping] = useState(false);
 
-	const getData = () => {
-		const client = new DataClient("http://localhost:8080")
-		const res = client.tweetStream(new TweetStreamRequest())
-		// @ts-ignore
-		res.on("data", x => console.log(x))
-	}
+  const pop = () => {
+    setPopping(true);
+    setTimeout(() => setPopping(false), 500);
+  };
 
-	useEffect(() => {
-		getData();
-	}, [])
+  const getData = () => {
+    const client = new DataClient("http://localhost:8080");
+    const res = client.tweetStream(new TweetStreamRequest());
+    // @ts-ignore
+    res.on("data", (x) => console.log(x));
+  };
 
-	const pop = () => {
-		setPopping(true);
-		setTimeout(() => setPopping(false), 500);
-	};
+  useEffect(() => {
+    getData();
+  }, []);
 
-	const buttonClass = function (active: boolean) {
-		return active
-			? "font-bold border-b-1 w-1/2 bg-white hover:bg-slate-100 bluesht transition-all duration-200"
-			: "border-b-1 w-1/2 bg-white hover:bg-slate-100 bluesht-disabled transition-all";
-	};
+  const buttonClass = function (active: boolean) {
+    return active
+      ? "font-bold w-1/2 bg-white hover:bg-slate-100 bluesht transition-all duration-200"
+      : "w-1/2 bg-white hover:bg-slate-100 bluesht-disabled transition-all";
+  };
 
-	const popSrc = () => {
-		return !popping ? "/close.gif" : "/pop-cat.gif";
-	};
+  const popSrc = () => {
+    return !popping ? "/close.gif" : "/pop-cat.gif";
+  };
 
-	return (
-		<div className="topo-bg w-full min-h-screen flex flex-col items-center ">
+  return (
+    <div className="topo-bg w-full min-h-screen flex flex-col items-center">
       {/* top level container 
       under create two subcontainers
       one has data and one has the buttons
       apply sticky to buttons and ur good */}
-			<img className="w-20 h-20" src={popSrc()} />
-			<p className="ml-4 font-bold text-5xl">FreeBird</p>
-			<button onClick={(_) => pop()}>Test</button>
-			<div className="bg-white mt-2 flex flex-col w-5/12 border-1 border-b-0 drop-shadow-lg">
-				{/* buttons */}
-				<div className="flex justify-between">
-					<button
-						className={buttonClass(toggle)}
-						onClick={(_) => {
-							setToggle(!toggle);
-						}}
-					>
-						<div className="pt-4 pb-3">Companies</div>
-					</button>
-					<button
-						className={buttonClass(!toggle)}
-						onClick={(_) => {
-							setToggle(!toggle);
-						}}
-					>
-						<div className="pt-4 pb-3">People</div>
-					</button>
-				</div>
-				<div className="w-full">
-					<Card></Card>
-					<Card></Card>
-					<Card></Card>
-					<Card></Card>
-					<Card></Card>
-					<Card></Card>
-				</div>
-			</div>
-		</div>
-	);
+      <header className="bg-white flex flex-col items-center border-1 w-5/12 sticky top-0 z-50">
+        <div className="flex flex-col items-center w-full">
+          <img className="w-20 h-20" src={popSrc()} />
+          <button onClick={(_) => pop()} className="ml-4 font-bold text-4xl">
+            FreeBird
+          </button>
+        </div>
+
+        <div className="bg-white flex flex-col w-full">
+          {/* buttons */}
+          <div className="flex justify-between">
+            <button
+              className={buttonClass(toggle)}
+              onClick={(_) => {
+                setToggle(!toggle);
+              }}
+            >
+              <div className="pt-4 pb-3">Companies</div>
+            </button>
+            <button
+              className={buttonClass(!toggle)}
+              onClick={(_) => {
+                setToggle(!toggle);
+              }}
+            >
+              <div className="pt-4 pb-3">People</div>
+            </button>
+          </div>
+        </div>
+      </header>
+      <div className="bg-white flex flex-col border-1 border-t-0 border-b-0 w-5/12 relative">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Card.tsx
+++ src/Card.tsx
@@ -16,7 +16,7 @@ export default function Card() {
 		<div>
 			<div className="pt-2 pb-4 w-full flex flex-row border-b-1 gap-x-3 px-4">
 				<div className="flex flex-row justify-center gap-x-4 items-center">
-					<div className="w-16 h-16 bg-black rounded-full self-start"></div>
+					<img src="https://en.wikipedia.org/wiki/Kanye_West#/media/File:Kanye_West_at_the_2009_Tribeca_Film_Festival_(crop_2).jpg" className="w-16 h-16 rounded-full self-start"></img>
 				</div>
 				<div className="flex flex-1 flex-col justify-between gap-y-1">
 					<div className="flex flex-row justify-between">
@@ -42,31 +42,31 @@ export default function Card() {
 					</div>
 
 					<div className="grid grid-cols-3 gap-3 pr-8">
-						<div className="p-2 flex flex-col justify-between border-1 gap-x-0.5">
-							<span className="tweet-color text-xs">Sentiment score:</span>
+						<div className="p-0.5 flex flex-col justify-between border-1 gap-x-0.5">
+							<span className="ml-1 tweet-color text-xs">Sentiment score:</span>
 							<span className={general(num, 0)}>{num}</span>
 						</div>
-						<div className="p-2 flex flex-col justify-between border-1 gap-x-0.5">
-							<span className="tweet-color text-xs">Positive tweets:</span>
+						<div className="p-0.5 flex flex-col justify-between border-1 gap-x-0.5">
+							<span className="ml-1 tweet-color text-xs">Positive tweets:</span>
 							<span className="text-center text-xl">123</span>
 						</div>
-						<div className="p-2 flex flex-col justify-between border-1 gap-x-0.5">
-							<span className="tweet-color text-xs">Negative tweets:</span>
+						<div className="p-0.5 flex flex-col justify-between border-1 gap-x-0.5">
+							<span className="ml-1 tweet-color text-xs">Negative tweets:</span>
 							<span className="text-center text-xl">123</span>
 						</div>
 
-						<div className="p-2 flex flex-col justify-between border-1 gap-x-0.5">
-							<span className="tweet-color text-xs">Total tweets:</span>
+						<div className="p-0.5 flex flex-col justify-between border-1 gap-x-0.5">
+							<span className="ml-1 tweet-color text-xs">Total tweets:</span>
 							<span className="text-center text-xl">123</span>
 						</div>
-						<div className="p-2 flex flex-col justify-between border-1 gap-x-0.5">
-							<span className="tweet-color text-xs">Tweet velocity:</span>
+						<div className="p-0.5 flex flex-col justify-between border-1 gap-x-0.5">
+							<span className="ml-1 tweet-color text-xs">Tweet velocity:</span>
 							<span className="text-center text-xl">123</span>
 						</div>
 					</div>
 				</div>
 			</div>
-            { show && <CardDetails/>}
+			{show && <CardDetails />}
 		</div>
 	);
 }
